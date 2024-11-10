544e8a482196a52b2ab9555ac1cd2ffd830071ef
laipi <wtf1317696@gmail.com>
2024-11-10 16:51:38 +0800

    抓取數據(尚未完成)

--- stock_app/src/collect.py
+++ stock_app/src/collect.py
@@ -14,3 +14,15 @@ class collect:
         # 檢查路徑
         if not os.path.exists(self.data_file):
             os.makedirs(self.data_file)
+
+    def collection(self, stock_num, start_date="2021-01-01", end_date=None):
+        # 讀取時間
+        end_date = datetime.now.strftime("%Y-%M-%D")
+
+    try:
+        stock = yf.Ticker(stock_num)
+        DataFrame = stock.history(start=start_date, end=end_date)
+        if DataFrame.empty:
+            print("不是哥們，找不到數據(資料框為空)")
+            return None
+        file_path = os.path.join(self.data_file)
