c77ae3e256b4d97928ac4eba39d58e4c6814a9b8
laipi <wtf1317696@gmail.com>
2024-11-10 15:09:58 +0800

    初始化資料夾

--- stock_app/src/collect.py
+++ stock_app/src/collect.py
@@ -1,1 +1,16 @@
 # 數據收集模組
+
+import yfinace as yf
+import pandas as pd
+import os
+from datetime import datetime
+
+
+class collect:
+
+    def __init__(self, data_file="data/stock_data"):
+        # 初始化數具資料夾
+        self.data_file = data_file
+        # 檢查路徑
+        if not os.path.exists(self.data_file):
+            os.makedirs(self.data_file)
